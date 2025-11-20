1353cb63340f560e198f1a932a3931bcc250a003
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2025-11-20 11:25:44 +0600

    Create toast_message.dart

---
+++ lib/common/widget/overlay/toast_message.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rapid/flutter_rapid.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+
+class ShowToast{
+  static success(String message){
+    Widget toastWidget = Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.green,
+      ),
+      child: ListTile(
+        minLeadingWidth: 0,
+        dense: true,
+        //leading: const Icon(Icons.close_rounded, color: Colors.white),
+        title: Text(message, style: const TextStyle(color: Colors.white, fontSize: 16), textAlign: TextAlign.center,),
+      ),
+    );
+
+    FToast fToast = FToast();
+    fToast.init(Get.overlayContext!);
+    fToast.removeCustomToast();
+    fToast.showToast(
+      toastDuration: const Duration(milliseconds: 1500),
+      child: toastWidget,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+  static error(String message, {int? milliseconds}){
+    Widget toastWidget = Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.red,
+      ),
+      child: ListTile(
+        minLeadingWidth: 0,
+        dense: true,
+        //leading: const Icon(Icons.close_rounded, color: Colors.white),
+        title: Text(message, style: const TextStyle(color: Colors.white, fontSize: 16),textAlign: TextAlign.center),
+      ),
+    );
+
+    FToast fToast = FToast();
+    fToast.init(Get.overlayContext!);
+    fToast.removeCustomToast();
+    fToast.showToast(
+      toastDuration: Duration(milliseconds: milliseconds??1500),
+      child: toastWidget,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+}
