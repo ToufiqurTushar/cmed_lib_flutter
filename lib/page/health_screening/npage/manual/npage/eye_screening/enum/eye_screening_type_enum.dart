f682d257cfb86ceb24e7e15a3034ca4a264b48b0
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2026-06-03 18:19:46 +0600

    translataion

--- lib/page/health_screening/npage/manual/npage/eye_screening/enum/eye_screening_type_enum.dart
+++ lib/page/health_screening/npage/manual/npage/eye_screening/enum/eye_screening_type_enum.dart
@@ -1,12 +1,12 @@
 import 'package:cmed_lib_flutter/common/helper/utils.dart';
 
 enum EyeScreeningTypeEnum {
-  NEAR_VISION_BOTH_EYE(1,'N8', "label_near_vision_test", 'Near Visual Acuity', 'N.B: If your near visual acuity is more than N8, you should consult with an eye doctor soon'),
-  FAR_VISION_DISTANCE_1(2,'', "label_distance_vision_test", 'Distance Visual Acuity', 'N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
-  ILLITERATE_TEST(3,'', "Distance Vision's Illiterate Test", 'Distance Visual Acuity', 'N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
-  CHILDREN_EYE_TEST(4,'', "Distance Vision's Children Test", 'Distance Visual Acuity','N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
-  COLOR_BLIND_TEST(5,'', "label_color_blind_test", 'Color Blindness Test', 'Possible Red-green deficiency. Please contact with an eye care professional soon'),
-  CONTRAST_TEST(6,'2.10 - 2.25', "label_color_contrast_test", 'Contrast Sensitivity Test', 'Your Score is : 0 & you have contrast sensitivity impairment.');
+  NEAR_VISION_BOTH_EYE(1,'N8', "label_near_vision_test", 'label_near_vision_test', 'N.B: If your near visual acuity is more than N8, you should consult with an eye doctor soon'),
+  FAR_VISION_DISTANCE_1(2,'', "label_distance_vision_test", 'label_distance_vision_test', 'N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
+  ILLITERATE_TEST(3,'', "Distance Vision's Illiterate Test", 'label_distance_vision_test', 'N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
+  CHILDREN_EYE_TEST(4,'', "Distance Vision's Children Test", 'label_distance_vision_test','N.B: If your distance visual acuity is more than 6/9, you should consult with an eye doctor soon'),
+  COLOR_BLIND_TEST(5,'', "label_color_blind_test", 'label_color_blind_test', 'Possible Red-green deficiency. Please contact with an eye care professional soon'),
+  CONTRAST_TEST(6,'2.10 - 2.25', "label_color_contrast_test", 'label_color_contrast_test', 'Your Score is : 0 & you have contrast sensitivity impairment.');
 
   const EyeScreeningTypeEnum(this.value, this.normalValue, this.nameEn, this.titleEn, this.suggestionEn);
 
--- lib/page/i4we/healthy_days/healthy_days_i18n.dart
+++ lib/page/i4we/healthy_days/healthy_days_i18n.dart
@@ -2,39 +2,39 @@ class HealthyDaysI18N {
   static Map<String, Map<String, String>> getTranslations() {
     return {
       "en_US": {
-        "Would you say that in general, your health is ?": "Would you say that in general, your health is ?",
+        "Would you say that in general your health in the last 15 Days ?": "Would you say that in general your health in the last 15 Days ?",
         "Excellent": "Excellent",
         "No Yield": "No Yield",
         "Your Healthy days record has been successfully reported. For further support, please Reach out to the CHE/ Nurse.": "Your Healthy days record has been successfully reported. For further support, please Reach out to the CHE/ Nurse.",
-        "Very Good": "Very Good",
+        "Verygood": "Verygood",
         "Low Yield": "Low Yield",
         "Good": "Good",
         "Medium Yield": "Medium Yield",
         "Fair": "Fair",
         "High Yield": "High Yield",
         "Poor": "Poor",
-        "In the past 30 days, how many days was your physical health not good?": "In the past 30 days, how many days was your physical health not good? ",
+        "Think about your physical health, which includes physical illness and injury. For how many days during the past 15 days was your physical health not good?": "Think about your physical health, which includes physical illness and injury. For how many days during the past 15 days was your physical health not good?",
         "0-30": "0-30",
-        "In the past 30 days, how many days was your mental health not good?": "In the past 30 days, how many days was your mental health not good? ",
-        "During the past 30 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?": "During the past 30 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?",
-        "Healthy Days": "Healthy Days"
+        "Now think about your mental health, like stress, depression, and anxiety problems with emotions, for how many days during the past 30 days was your mental health not good?": "Now think about your mental health, like stress, depression, and anxiety problems with emotions, for how many days during the past 30 days was your mental health not good?",
+        "During the past 15 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?": "During the past 15 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?",
+        "Healthy Days ": "Healthy Days "
       },
       "kn_IN": {
-        "Would you say that in general, your health is ?": "ಸಾಮಾನ್ಯವಾಗಿ ನಿಮ್ಮ ಆರೋಗ್ಯ ಹೇಗಿದೆ ಎಂದು ನೀವು ಹೇಳುತ್ತೀರಾ?",
+        "Would you say that in general your health in the last 15 Days ?": "ಕಳೆದ 15 ದಿನಗಳಲ್ಲಿ ನಿಮ್ಮ ಆರೋಗ್ಯ ಹೇಗಿತ್ತು ಎಂದು ನೀವು ಹೇಳುತ್ತೀರಾ?",
         "Excellent": "ಅತ್ಯುತ್ತಮ",
         "No Yield": "ಇಳುವರಿ ಇಲ್ಲ",
         "Your Healthy days record has been successfully reported. For further support, please Reach out to the CHE/ Nurse.": "ನಿಮ್ಮ ಆರೋಗ್ಯಕರ ದಿನಗಳ ದಾಖಲೆಯನ್ನು ಯಶಸ್ವಿಯಾಗಿ ವರದಿ ಮಾಡಲಾಗಿದೆ. ಹೆಚ್ಚಿನ ಬೆಂಬಲಕ್ಕಾಗಿ, ದಯವಿಟ್ಟು CHE/ ನರ್ಸ್ ಅವರನ್ನು ಸಂಪರ್ಕಿಸಿ.",
-        "Very Good": "ತುಂಬಾ ಒಳ್ಳೆಯದು",
+        "Verygood": "ತುಂಬಾ ಒಳ್ಳೆಯದು",
         "Low Yield": "ಕಡಿಮೆ ಇಳುವರಿ",
         "Good": "ಒಳ್ಳೆಯದು",
         "Medium Yield": "ಮಧ್ಯಮ ಇಳುವರಿ",
         "Fair": "ನ್ಯಾಯೋಚಿತ",
         "High Yield": "ಅಧಿಕ ಇಳುವರಿ",
         "Poor": "ಕಳಪೆ",
-        "In the past 30 days, how many days was your physical health not good?": "ಕಳೆದ 30 ದಿನಗಳಲ್ಲಿ, ನಿಮ್ಮ ದೈಹಿಕ ಆರೋಗ್ಯ ಎಷ್ಟು ದಿನ ಚೆನ್ನಾಗಿರಲಿಲ್ಲ?",
+        "Think about your physical health, which includes physical illness and injury. For how many days during the past 15 days was your physical health not good?": "ದೈಹಿಕ ಅನಾರೋಗ್ಯ ಮತ್ತು ಗಾಯ ಸೇರಿದಂತೆ ನಿಮ್ಮ ದೈಹಿಕ ಆರೋಗ್ಯದ ಬಗ್ಗೆ ಯೋಚಿಸಿ. ಕಳೆದ 15 ದಿನಗಳಲ್ಲಿ ಎಷ್ಟು ದಿನಗಳವರೆಗೆ ನಿಮ್ಮ ದೈಹಿಕ ಆರೋಗ್ಯ ಚೆನ್ನಾಗಿರಲಿಲ್ಲ?",
         "0-30": "0-30",
-        "In the past 30 days, how many days was your mental health not good?": "ಕಳೆದ 30 ದಿನಗಳಲ್ಲಿ, ನಿಮ್ಮ ಮಾನಸಿಕ ಆರೋಗ್ಯ ಎಷ್ಟು ದಿನಗಳು ಚೆನ್ನಾಗಿರಲಿಲ್ಲ?",
-        "During the past 30 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?": "ಕಳೆದ 30 ದಿನಗಳಲ್ಲಿ, ದೈಹಿಕ ಅಥವಾ ಮಾನಸಿಕ ಅಸ್ವಸ್ಥತೆಯು ನಿಮ್ಮ ಸಾಮಾನ್ಯ ಚಟುವಟಿಕೆಗಳಾದ ಸ್ವ-ಆರೈಕೆ, ಕೆಲಸ ಅಥವಾ ಮನರಂಜನೆಯನ್ನು ಎಷ್ಟು ದಿನಗಳವರೆಗೆ ಮಾಡದಂತೆ ತಡೆಯಿತು?",
+        "Now think about your mental health, like stress, depression, and anxiety problems with emotions, for how many days during the past 30 days was your mental health not good?": "ಈಗ ನಿಮ್ಮ ಮಾನಸಿಕ ಆರೋಗ್ಯದ ಬಗ್ಗೆ ಯೋಚಿಸಿ, ಉದಾಹರಣೆಗೆ ಒತ್ತಡ, ಖಿನ್ನತೆ ಮತ್ತು ಭಾವನೆಗಳೊಂದಿಗೆ ಆತಂಕದ ಸಮಸ್ಯೆಗಳು, ಕಳೆದ 30 ದಿನಗಳಲ್ಲಿ ಎಷ್ಟು ದಿನಗಳವರೆಗೆ ನಿಮ್ಮ ಮಾನಸಿಕ ಆರೋಗ್ಯ ಚೆನ್ನಾಗಿರಲಿಲ್ಲ?",
+        "During the past 15 days, for about how many days did poor physical or mental health keep you from doing your usual activities, such as self-care, work, or recreation?": "ಕಳೆದ 15 ದಿನಗಳಲ್ಲಿ, ದೈಹಿಕ ಅಥವಾ ಮಾನಸಿಕ ಅಸ್ವಸ್ಥತೆಯು ನಿಮ್ಮ ಸಾಮಾನ್ಯ ಚಟುವಟಿಕೆಗಳಾದ ಸ್ವ-ಆರೈಕೆ, ಕೆಲಸ ಅಥವಾ ಮನರಂಜನೆಯನ್ನು ಎಷ್ಟು ದಿನಗಳವರೆಗೆ ಮಾಡದಂತೆ ತಡೆಯಿತು?",
         "Healthy Days ": "ಆರೋಗ್ಯಕರ ದಿನಗಳು"
       }
     };
