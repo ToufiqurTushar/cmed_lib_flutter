51d7b07ad2da8f0c7d24922a0c23c73b8210265d
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2026-07-12 18:15:23 +0600

    Create cmed_bottom_button.dart

---
+++ lib/common/widget/cmed_bottom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+Widget CMEDBottomButton({
+  required String label,
+  required VoidCallback? onPressed,
+  Color? backgroundColor,
+  Color? textColor,
+}) {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: SizedBox(
+      width: double.infinity,
+      height: 52,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor?? Colors.lightBlue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(26.0),
+          ),
+          elevation: 0,
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: textColor??Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ),
+  );
+}
