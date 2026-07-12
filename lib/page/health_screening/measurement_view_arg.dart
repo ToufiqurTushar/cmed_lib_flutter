cc13af781f600f98c3cda78d4d9dfc2e96fd3cba
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2026-07-12 13:39:10 +0600

    Update measurement_view_arg.dart

--- lib/page/health_screening/measurement_view_arg.dart
+++ lib/page/health_screening/measurement_view_arg.dart
@@ -1,15 +1,21 @@
 class MeasurementViewArg {
   MeasurementViewArg({
-      this.isSusasthoV2,});
+      this.isSusasthoV2,
+      this.isAuto,
+
+  });
 
   MeasurementViewArg.fromJson(dynamic json) {
     isSusasthoV2 = json['isSusasthoV2'];
+    isAuto = json['isAuto'];
   }
   bool? isSusasthoV2;
+  bool? isAuto;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['isSusasthoV2'] = isSusasthoV2;
+    map['isAuto'] = isAuto;
     return map;
   }
 
